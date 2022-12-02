e06f5de166b1b2522789d380de712389dd89c5ef
jst-kakiuchi <yuki.kakiuchi@stream.co.jp>
2022-12-03 04:01:20 +0900

    update:#15 configファイルの修正

--- config/config.go
+++ config/config.go
@@ -5,15 +5,20 @@ import (
 )
 
 type Config struct {
-	Env        string `env:"GO_ENV" envDefault:"production"`
-	Port       int    `env:"PORT" envDefault:"80"`
-	DBHost     string `env:"DB_HOST" envDefault:"db"`
-	DBPort     int    `env:"DB_PORT" envDefault:"3306"`
-	DBUser     string `env:"DB_USER" envDefault:"admin"`
-	DBPassword string `env:"DB_PASSWORD" envDefault:"password"`
-	DBName     string `env:"DB_NAME" envDefault:"point_app"`
-	RedisHost  string `env:"REDIS_HOST" envDefault:"db"`
-	RedisPort  int    `env:"REDIS_PORT" envDefault:"36379"`
+	Env               string `env:"GO_ENV" envDefault:"production"`
+	Port              int    `env:"PORT" envDefault:"80"`
+	DBHost            string `env:"DB_HOST" envDefault:"db"`
+	DBPort            int    `env:"DB_PORT" envDefault:"3306"`
+	DBUser            string `env:"DB_USER" envDefault:"admin"`
+	DBPassword        string `env:"DB_PASSWORD" envDefault:"password"`
+	DBName            string `env:"DB_NAME" envDefault:"point_app"`
+	RedisHost         string `env:"REDIS_HOST" envDefault:"db"`
+	RedisPort         int    `env:"REDIS_PORT" envDefault:"36379"`
+	AWSEndpoint       string `env:"AWS_ENDPOINT" envDefault:""`
+	AWSId             string `env:"AWS_ACCESS_KEY_ID" envDefault:"accesskey"`
+	AWSSecret         string `env:"AWS_SECRET_KEY" envDefault:"secretkey"`
+	AWSRegion         string `env:"AWS_REGION" envDefault:"ap-northeast-1"`
+	SenderMailAddress string `env:"SENDER_MAIL_ADDRESS" envDefault:"sample@sample.com"`
 }
 
 // 環境変数の構造体を返却
