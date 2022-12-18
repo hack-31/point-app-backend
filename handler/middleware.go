eceb0727ccc86c8668477a4a90449a2c51cd9e34
jst-kakiuchi <yuki.kakiuchi@stream.co.jp>
2022-12-19 01:07:08 +0900

    add:#25 認証ミドルウェアの追加

---
+++ handler/middleware.go
@@ -0,0 +1,23 @@
+package handler
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hack-31/point-app-backend/auth"
+)
+
+// 認証ミドルウェア
+func AuthMiddleware(j *auth.JWTer) gin.HandlerFunc {
+	return gin.HandlerFunc(func(ctx *gin.Context) {
+		log.Print("AuthMiddleware")
+
+		if err := j.FillContext(ctx); err != nil {
+			log.Print(err.Error())
+			APIResponse(ctx, "アクセストークンが無効です。再ログインしてください。", http.StatusUnauthorized, http.MethodPost, nil)
+			return
+		}
+		ctx.Next()
+	})
+}
