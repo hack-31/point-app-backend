b86da6ef51b772c924fc4b24968ea3dae1383cac
jst-kakiuchi <--global>
2023-01-27 00:35:02 +0900

    update:#55 errorの定義

--- repository/error.go
+++ repository/error.go
@@ -8,11 +8,14 @@ const (
 	// ErrCodeMySQLDuplicateEntry はMySQL系のDUPLICATEエラーコード
 	// https://dev.mysql.com/doc/mysql-errors/8.0/en/server-error-reference.html
 	// Error number: 1062; Symbol: ER_DUP_ENTRY; SQLSTATE: 23000
-	ErrCodeMySQLDuplicateEntry = 1062
+	ErrCodeMySQLDuplicateEntry  = 1062
+	ErrCodeMySQLNoReferencedRow = 1452
 )
 
 var (
-	ErrAlreadyEntry    = errors.New("登録済みのメールアドレスは登録できません。")
-	ErrNotFoundSession = errors.New("確認コードまたは、セッションキーが無効です。")
-	ErrNotMatchLogInfo = errors.New("メールアドレスまたは、パスワードが異なります。")
+	ErrAlreadyEntry        = errors.New("登録済みのメールアドレスは登録できません。")
+	ErrNotFoundSession     = errors.New("確認コードまたは、セッションキーが無効です。")
+	ErrNotMatchLogInfo     = errors.New("メールアドレスまたは、パスワードが異なります。")
+	ErrNotUser             = errors.New("ユーザが存在しません。")
+	ErrHasNotSendablePoint = errors.New("送付可能ポイントが不足しています。")
 )
