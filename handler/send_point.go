9df2ce1f9af372cdb92f93e3946eb4a6c23db5ce
jst-kakiuchi <--global>
2023-01-27 01:12:46 +0900

    add:#55 ハンドラーとサービスの作成

---
+++ handler/send_point.go
@@ -0,0 +1,67 @@
+package handler
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/hack-31/point-app-backend/repository"
+)
+
+type SendPoint struct {
+	Service SendPointService
+}
+
+func NewSendPoint(s SendPointService) *SendPoint {
+	return &SendPoint{Service: s}
+}
+
+// ポイント送付ハンドラー
+//
+// @param ctx ginContext
+func (sp *SendPoint) ServeHTTP(ctx *gin.Context) {
+	// ユーザのパラメータ検証
+	var input struct {
+		ToUserId  int `json:"toUserId"`
+		SendPoint int `json:"sendPoint"`
+	}
+
+	const errTitle = "送付エラー"
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ErrResponse(ctx, http.StatusBadRequest, errTitle, err.Error())
+		return
+	}
+
+	// バリデーション
+	err := validation.ValidateStruct(&input,
+		validation.Field(
+			&input.ToUserId,
+			validation.Required,
+		),
+		validation.Field(
+			&input.SendPoint,
+			validation.Required,
+		),
+	)
+	if err != nil {
+		ErrResponse(ctx, http.StatusBadRequest, errTitle, err.Error())
+		return
+	}
+
+	// ポイント送付
+	if err := sp.Service.SendPoint(ctx, input.ToUserId, input.SendPoint); err != nil {
+		if errors.Is(err, repository.ErrNotUser) {
+			ErrResponse(ctx, http.StatusNotFound, errTitle, repository.ErrNotUser.Error())
+			return
+		}
+		if errors.Is(err, repository.ErrHasNotSendablePoint) {
+			ErrResponse(ctx, http.StatusBadRequest, errTitle, repository.ErrHasNotSendablePoint.Error())
+			return
+		}
+		ErrResponse(ctx, http.StatusInternalServerError, errTitle, err.Error())
+		return
+	}
+
+	APIResponse(ctx, http.StatusCreated, "ポイントの送信に成功しました。", nil)
+}
