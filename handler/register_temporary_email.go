68aa57b4a5d4ea01f558bfc22c46804fb450cd24
daikiku10 <soccerdk19.910@gmail.com>
2023-04-02 11:48:33 +0900

    update:#81 handlerバリデーションチェックの実装

--- handler/register_temporary_email.go
+++ handler/register_temporary_email.go
@@ -1,9 +1,11 @@
 package handler
 
 import (
-	"io/ioutil"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
 )
 
 type RegisterTemporaryEmail struct {
@@ -18,9 +20,28 @@ func NewRegisterTemporaryEmailHandler() *RegisterTemporaryEmail {
 //
 // @param ctx ginContext
 func (ru *RegisterTemporaryEmail) ServeHTTP(ctx *gin.Context) {
-	body, err := ioutil.ReadAll(ctx.Request.Body)
-	if err != nil {
-		println("エラー")
+	const errTitle = "メール仮登録エラー"
+
+	var input struct {
+		Email string `json:"email"`
 	}
-	println(string(body))
+
+	// ユーザーから正しいパラメータでポストデータが送られていない
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ErrResponse(ctx, http.StatusBadRequest, errTitle, err.Error())
+		return
+	}
+
+	// バリデーション検証
+	err := validation.ValidateStruct(&input,
+		validation.Field(
+			&input.Email,
+			validation.Required,
+			validation.Length(1, 256),
+			is.Email,
+		))
+	if err != nil {
+		ErrResponse(ctx, http.StatusBadRequest, errTitle, err.Error())
+		return
+	}
 }
