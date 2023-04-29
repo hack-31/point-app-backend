e544c033326728ab5c542ea0ac309d6d019072b3
daikiku10 <soccerdk19.910@gmail.com>
2023-04-29 23:28:27 +0900

    update:#82 handler層 バリデーションチェックの実装

--- handler/update_email.go
+++ handler/update_email.go
@@ -1,6 +1,11 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+)
 
 type UpdateEmail struct{}
 
@@ -12,5 +17,38 @@ func NewUpdateEmailHandler() *UpdateEmail {
 //
 // @param ctx ginContext
 func (ue *UpdateEmail) ServeHTTP(ctx *gin.Context) {
-	println("テスト")
+	// const mailErrTitle = "メールアドレス更新エラー"
+	const paramErrTitle = "パラメータエラー"
+
+	var input struct {
+		TemporaryEmailID string `json:"temporaryEmailId"`
+		ConfirmCode      string `json:"confirmCode"`
+	}
+
+	// ユーザーから正しいパラメータで送られているか確認
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ErrResponse(ctx, http.StatusBadRequest, paramErrTitle, err.Error())
+		return
+	}
+
+	// バリデーションチェック
+	err := validation.ValidateStruct(&input,
+		validation.Field(
+			&input.TemporaryEmailID,
+			validation.Required,
+		),
+		validation.Field(
+			&input.ConfirmCode,
+			validation.Required,
+			validation.Length(4, 4),
+		),
+	)
+	if err != nil {
+		ErrResponse(ctx, http.StatusBadRequest, paramErrTitle, err.Error())
+		return
+	}
+
+	// 成功レスポンス
+	APIResponse(ctx, http.StatusCreated, "更新が完了しました。", nil)
+
 }
