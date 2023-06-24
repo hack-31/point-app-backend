53ec31f4d289f4d7532aa8e731a7310a01120083
Yuki-TU <k.yuki.90219@icloud.com>
2023-06-25 03:19:19 +0900

    add:#91 お知らせ管理のmodelの定義

---
+++ domain/model/notification.go
@@ -0,0 +1,19 @@
+package model
+
+import (
+	"time"
+)
+
+type NotificationID int64
+
+type Notification struct {
+	ID          NotificationID `json:"id" db:"id"`
+	TypeID      int            `json:"notificationTypeId" db:"notification_type_id"`
+	Title       string         `json:"title" db:"title"`
+	Description string         `json:"description" db:"description"`
+	ToUserID    UserID         `json:"toUserId" db:"to_user_id"`
+	FromUserID  UserID         `json:"fromUserId" db:"from_user_id"`
+	IsChecked   bool           `json:"isChecked" db:"is_checked"`
+	CreatedAt   time.Time      `json:"createdAt" db:"created_at"`
+}
+type Notifications []*Notification
