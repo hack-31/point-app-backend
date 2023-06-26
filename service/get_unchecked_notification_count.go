33e2ed7324ade20caaa9965937e12901626eb0b7
Yuki-TU <k.yuki.90219@icloud.com>
2023-06-26 22:37:18 +0900

    add:#95 サービスの作成

---
+++ service/get_unchecked_notification_count.go
@@ -0,0 +1,63 @@
+package service
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hack-31/point-app-backend/auth"
+	"github.com/hack-31/point-app-backend/domain"
+	"github.com/hack-31/point-app-backend/domain/model"
+	"github.com/hack-31/point-app-backend/repository"
+	"github.com/jmoiron/sqlx"
+)
+
+type GetUncheckedNotificationCount struct {
+	DB        repository.Queryer
+	Cache     domain.Cache
+	NotifRepo domain.NotificationRepo
+}
+
+func NewGetUncheckedNotificationCount(db *sqlx.DB, cache domain.Cache, repo *repository.Repository) *GetUncheckedNotificationCount {
+	return &GetUncheckedNotificationCount{DB: db, Cache: cache, NotifRepo: repo}
+}
+
+// お知らせ数を確認し、かつ、お知らせ通知をサブスクライブ
+//
+// @params ctx コンテキスト
+//
+// @return
+// ユーザ一覧
+func (gn *GetUncheckedNotificationCount) GetUncheckedNotificationCount(ctx *gin.Context, notificationCntChan chan<- int) (int, error) {
+	// ユーザID確認
+	u, _ := ctx.Get(auth.UserID)
+	userID := u.(model.UserID)
+
+	// お知らせ数の確認
+	cnt, err := gn.NotifRepo.GetUncheckedNotificationCount(ctx, gn.DB, userID)
+	if err != nil {
+		return 0, fmt.Errorf("faild to get unchecked notification count from db: %w", err)
+	}
+
+	// お知らせ通知をサブスクライブ
+	go func() {
+		payloadChan, err := gn.Cache.Subscribe(ctx, fmt.Sprintf("notification:%d", userID))
+		if err != nil {
+			return
+		}
+		for {
+			select {
+			case <-ctx.Request.Context().Done():
+				return
+			case <-payloadChan:
+				// お知らせの通知が来たら、お知らせテーブルよりお知らせ数取得
+				cnt, err := gn.NotifRepo.GetUncheckedNotificationCount(ctx, gn.DB, userID)
+				if err != nil {
+					return
+				}
+				notificationCntChan <- cnt
+			}
+		}
+	}()
+
+	return cnt, nil
+}
