f0ef3d9704fe6c9e2d7631e511e049bcbe90a65e
Yuki-TU <k.yuki.90219@icloud.com>
2024-02-29 23:41:11 +0900

    add:#128 ClockにAsiaの設定ができるようにした

--- utils/clock/clock.go
+++ utils/clock/clock.go
@@ -1,6 +1,7 @@
 package clock
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -14,8 +15,24 @@ func (r RealClocker) Now() time.Time {
 	return time.Now()
 }
 
-type FixedClocker struct{}
+type FixedClocker struct {
+	IsAsia bool
+}
 
 func (fc FixedClocker) Now() time.Time {
-	return time.Date(2022, 5, 10, 12, 34, 56, 0, time.UTC)
+	// UTCで指定された日時
+	utcTime := time.Date(2022, 5, 10, 12, 34, 56, 0, time.UTC)
+
+	if fc.IsAsia {
+		// Asia / Tokyoのタイムゾーンに変換
+		loc, err := time.LoadLocation("Asia/Tokyo")
+		if err != nil {
+			fmt.Println("Error:", err)
+			return time.Time{}
+		}
+		tokyoTime := utcTime.In(loc)
+		return tokyoTime
+	}
+
+	return utcTime
 }
