f991b81d776d0bcab4000b435b674acdb1f397c2
Yuki-TU <k.yuki.90219@icloud.com>
2024-03-01 00:05:50 +0900

    add:#128 デフォルトデータの挿入のコード

---
+++ repository/testdata/user.go
@@ -0,0 +1,86 @@
+package testdata
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hack-31/point-app-backend/domain/model"
+	"github.com/hack-31/point-app-backend/utils/clock"
+	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/assert"
+)
+
+// ユーザーテーブルにデータを挿入する
+func Users(t *testing.T, ctx context.Context, con *sqlx.Tx, setter func(users model.Users)) model.Users {
+	t.Helper()
+	// ユーザ一覧を削除する
+	if _, err := con.ExecContext(ctx, `DELETE FROM delete_users;`); err != nil {
+		t.Logf("%v", err)
+	}
+	if _, err := con.ExecContext(ctx, `DELETE FROM users;`); err != nil {
+		t.Logf("%v", err)
+	}
+
+	c := clock.FixedClocker{
+		IsAsia: true,
+	}
+
+	// ユーザーデータのデフォルト値
+	users := model.Users{
+		{
+			FirstName:      "太郎",
+			FirstNameKana:  "タロウ",
+			FamilyName:     "本田",
+			FamilyNameKana: "ホンダ",
+			Email:          "honda.taro@sample.com",
+			Password:       "honda.pass",
+			SendingPoint:   0,
+			CreatedAt:      c.Now(),
+			UpdateAt:       c.Now(),
+		},
+		{
+			FirstName:      "葵",
+			FirstNameKana:  "あおい",
+			FamilyName:     "斉藤",
+			FamilyNameKana: "さいとう",
+			Email:          "saito.aoi@example.com",
+			Password:       "aoi.pass",
+			SendingPoint:   100,
+			CreatedAt:      c.Now(),
+			UpdateAt:       c.Now(),
+		},
+		{
+			FirstName:      "拓也",
+			FirstNameKana:  "たくや",
+			FamilyName:     "木村",
+			FamilyNameKana: "きむら",
+			Email:          "kimura.takuya@example.com",
+			Password:       "kimura.pass",
+			SendingPoint:   800,
+			CreatedAt:      c.Now(),
+			UpdateAt:       c.Now(),
+		},
+	}
+
+	// データの上書き
+	setter(users)
+
+	// データ道入
+	result, err := con.NamedExecContext(ctx, `
+		INSERT INTO users
+			(first_name, first_name_kana, family_name, family_name_kana, email, password, sending_point, created_at, update_at)
+		VALUES
+			(:first_name, :first_name_kana, :family_name, :family_name_kana, :email, :password, :sending_point, :created_at, :update_at);`, users)
+	assert.NoError(t, err)
+
+	// 自動発行されたユーザーIDを取得
+	id, err := result.LastInsertId()
+	assert.NoError(t, err)
+	fmt.Printf("id: %v\n", id)
+	users[0].ID = model.UserID(id)
+	users[1].ID = model.UserID(id + 1)
+	users[2].ID = model.UserID(id + 2)
+
+	return users
+}
