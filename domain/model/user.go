a2552a5b464548be7603abb7a8f4bdaf9b638944
Yuki-TU <k.yuki.90219@icloud.com>
2024-03-25 23:55:55 +0900

    remove:#132 不要なプロパティ削除

--- domain/model/user.go
+++ domain/model/user.go
@@ -7,17 +7,16 @@ import (
 type UserID int64
 
 type User struct {
-	ID               UserID    `json:"id" db:"id"`
-	FirstName        string    `json:"first_name" db:"first_name"`
-	FirstNameKana    string    `json:"first_name_kana" db:"first_name_kana"`
-	FamilyName       string    `json:"family_name" db:"family_name"`
-	FamilyNameKana   string    `json:"family_name_kana" db:"family_name_kana"`
-	Password         string    `json:"password" db:"password"`
-	SendingPoint     int       `json:"sending_point" db:"sending_point"`
-	Email            string    `json:"email" db:"email"`
-	CreatedAt        time.Time `json:"createdAt" db:"created_at"`
-	UpdateAt         time.Time `json:"updateAt" db:"update_at"`
-	AcquisitionPoint int       `json:"acquisition_point" db:"acquisition_point"`
+	ID             UserID    `json:"id" db:"id"`
+	FirstName      string    `json:"firstName" db:"first_name"`
+	FirstNameKana  string    `json:"firstNameKana" db:"first_name_kana"`
+	FamilyName     string    `json:"familyName" db:"family_name"`
+	FamilyNameKana string    `json:"familyName_kana" db:"family_name_kana"`
+	Password       string    `json:"password" db:"password"`
+	SendingPoint   int       `json:"sendingPoint" db:"sending_point"`
+	Email          string    `json:"email" db:"email"`
+	CreatedAt      time.Time `json:"createdAt" db:"created_at"`
+	UpdateAt       time.Time `json:"updateAt" db:"update_at"`
 }
 
 type Users []*User
