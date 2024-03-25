905b907b03f36ec3690f2be0a089e5efa6d12023
Yuki-TU <k.yuki.90219@icloud.com>
2024-03-25 23:42:25 +0900

    add:#132 取引テーブルレポ作成 * 獲得ポイントを取得

---
+++ domain/model/transaction.go
@@ -0,0 +1,15 @@
+package model
+
+import "time"
+
+type TransactionID int64
+
+type Transaction struct {
+	ID               TransactionID `json:"id" db:"id"`
+	ReceivingUserID  UserID        `json:"receivingUserId" db:"receiving_user_id"`
+	SendingUserID    UserID        `json:"sendingUserId" db:"sending_user_id"`
+	TransactionPoint int           `json:"transactionPoint" db:"transaction_point"`
+	TransactionAt    time.Time     `json:"transactionAt" db:"transaction_at"`
+}
+
+type Transactions []*Transaction
