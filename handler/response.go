6162f7d1a772c13dd1366204ad1b70781ee13631
Yuki-TU <k.yuki.90219@icloud.com>
2024-04-05 00:35:11 +0900

    add:#134 ログの登録

--- handler/response.go
+++ handler/response.go
@@ -1,6 +1,11 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hack-31/point-app-backend/middleware"
+)
 
 // クライアントへの返すレスポンスの型
 type Responses struct {
@@ -30,6 +35,16 @@ func APIResponse(ctx *gin.Context, StatusCode int, Message string, Data interfac
 		Data:       Data,
 	}
 	ctx.JSON(StatusCode, jsonResponse)
+
+	// ログに使う情報登録
+	js, err := json.Marshal(jsonResponse)
+	if err != nil {
+		panic("can not marshal")
+	}
+	middleware.NewLog().
+		Rsp(js).
+		Logging(ctx)
+
 }
 
 // エラーレスポンス作成
@@ -39,7 +54,7 @@ func APIResponse(ctx *gin.Context, StatusCode int, Message string, Data interfac
 // StatusCode ステータスコード
 // Title エラータイトル
 // Message メッセージ
-func ErrResponse(ctx *gin.Context, StatusCode int, Title, Message string) {
+func ErrResponse(ctx *gin.Context, StatusCode int, Title, Message string, Err error) {
 	res := ErrRes{
 		StatusCode: StatusCode,
 		Title:      Title,
@@ -47,4 +62,15 @@ func ErrResponse(ctx *gin.Context, StatusCode int, Title, Message string) {
 	}
 	defer ctx.AbortWithStatus(StatusCode)
 	ctx.JSON(StatusCode, res)
+
+	// ログに使う情報登録
+	js, err := json.Marshal(res)
+	if err != nil {
+		panic("can not marshal")
+	}
+	middleware.NewLog().
+		ErrCode(StatusCode).
+		Err(Err).
+		Rsp(js).
+		Logging(ctx)
 }
