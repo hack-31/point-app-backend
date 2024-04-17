ef8f00cef2af497fbbd87def63ad79d575470d7b
Yuki-TU <k.yuki.90219@icloud.com>
2024-04-17 21:22:20 +0900

    add:SD-136 DBの接続をモックするutilを作成

--- testutil/db.go
+++ testutil/db.go
@@ -1,17 +1,20 @@
 package testutil
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"os"
 	"testing"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"github.com/stretchr/testify/assert"
 )
 
 // テストにおいてデータベース接続する
+// 実際にデーターベース接続する
 func OpenDBForTest(t *testing.T) *sqlx.DB {
 	t.Helper()
 
@@ -37,4 +40,60 @@ func OpenDBForTest(t *testing.T) *sqlx.DB {
 	assert.NoError(t, err)
 
 	return xdb
+}
+
+// NewDBForTest はテスト用のDBを作成する
+//
+// 実際にはDBには接続しない
+func NewDBForMock(t *testing.T) (*sqlx.DB, sqlmock.Sqlmock) {
+	t.Helper()
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		assert.Fail(t, err.Error())
+	}
+
+	xdb := sqlx.NewDb(db, "sqlmock")
+	t.Cleanup(
+		func() { _ = xdb.Close() },
+	)
+	if err := xdb.Ping(); err != nil {
+		assert.Error(t, err)
+	}
+
+	return xdb, mock
+}
+
+// NewTxForMock はモック用のトランザクションを作成する
+//
+// 実際にはDBには接続しない
+//
+// begin, commitは正常系を返すようにモックする
+// 異常系をテストしたい場合は、NewDBForMockを使って自分でモックを作成する
+func NewTxForMock(t *testing.T, ctx context.Context) *sqlx.Tx {
+	t.Helper()
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		assert.Fail(t, err.Error())
+	}
+
+	xdb := sqlx.NewDb(db, "sqlmock")
+	t.Cleanup(
+		func() { _ = xdb.Close() },
+	)
+	if err := xdb.Ping(); err != nil {
+		assert.Error(t, err)
+	}
+
+	// トランザクションの開始、コミットをモックする
+	// 正常系を返す
+	mock.ExpectBegin()
+	mock.ExpectCommit()
+
+	mockTx, err := xdb.BeginTxx(ctx, nil)
+	if err != nil {
+		assert.Error(t, err)
+	}
+	return mockTx
 }
